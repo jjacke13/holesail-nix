d0bbdb06079a85a131e700840d822843e5b68ba0
Vaios Karastathis <156372486+jjacke13@users.noreply.github.com>
2024-10-26 15:20:08 +0300

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -5,15 +5,31 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     };
 
-    outputs = { self, nixpkgs, ... }:
+    outputs = { self, nixpkgs, ... } @ inputs:
     let
-        system = "aarch64-linux";
-        pkgs = import nixpkgs {inherit system; };
+       system = "aarch64-linux";
+       pkgs = import nixpkgs {inherit system; };
     in 
     {
-        packages = {
-                holesail = ./holesail.nix ;
-            };
+        packages.holesail = pkgs.buildNpmPackage rec {
+                                pname = "holesail";
+                                version = "1.7.3";
 
+                                src = pkgs.fetchFromGitHub {
+                                owner = "holesail";
+                                repo = pname;
+                                rev = "refs/tags/${version}";
+                                hash = "sha256-u7u+bljBEkbLWRrXMDZdgAe1xozx2rlqgnXSFxGqFVk=";
+                                };
+                                npmDepsHash = "sha256-lZEpP14sN62LOv85VsGEIWAHXQuRt6lfhbp/iGpffX4=";
+                                npmPackFlags = [ "--ignore-scripts" ];
+                                buildPhase = "echo 'No build phase required'";
+                                meta = {
+                                description = "Holesail!";
+                                homepage = "holesail.io";
+                                license = pkgs.lib.licenses.gpl3Only;
+                                maintainers = with pkgs.lib.maintainers; [ pkgs.lib.maintainers.jjacke13 ];
+                                };  
+        };
     };
 }
