5ca91dd5694f61344d48c86377417660442c1c7a
Vaios K <jjacke@proton.me>
2024-12-20 12:34:50 +0200

    Update to Holesail 1.8.4

--- flake.nix
+++ flake.nix
@@ -16,15 +16,15 @@
     packages = forAllSystems (system:{
       holesail = pkgs.${system}.buildNpmPackage rec {
         pname = "holesail";
-        version = "1.8.3";
+        version = "1.8.4";
         src = pkgs.${system}.fetchFromGitHub {
           owner = "holesail";
           repo = pname;
           rev = "refs/tags/${version}";
-          hash = "sha256-R487u6QX0eyNyJ9kRe5kXJ0HUgYIB9yGdXJGiuZ1keA=";
+          hash = "sha256-htaXKozyPoJEcxakCxsbIWgdHWMYyICYxG+Kv2uEdDc=";
         };
 
-        npmDepsHash = "sha256-LYr5hNEC5e6ddQVYrgmOX2N/hJcrseJGOq58i4UHOLU=";
+        npmDepsHash = "sha256-S/ugVA2l+seDSeWhN5fIDnFheWHnYD823J8VGofunvs=";
         npmPackFlags = [ "--ignore-scripts" ];
         buildPhase = "echo 'No build phase required'";
         meta = {
