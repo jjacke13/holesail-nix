8c765a2f35b27aaf1b4308a79d0785acb31a663f
Vaios K <jjacke@proton.me>
2025-01-11 22:23:27 +0200

    Update flake.nix
    added sever, client and respective nix-shells

--- flake.nix
+++ flake.nix
@@ -4,18 +4,37 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
-  outputs = { self, nixpkgs, systems, ... }@inputs:
-  let
-    pkgs = nixpkgs.legacyPackages;
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in
-  {
-    packages = eachSystem (system:{
-      holesail = import ./holesail.nix { pkgs = pkgs.${system}; };
-    });
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
 
-  };
-
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+        pack = self.packages.${system};
+      in
+      {
+        packages = rec {
+          holesail = import ./holesail.nix { inherit pkgs; };
+          holesail-server = import ./holesail-server.nix { inherit pkgs; };
+          holesail-client = import ./holesail-client.nix { inherit pkgs; };
+          default = holesail;
+        };
+        devShells = {
+          holesail = pkgs.mkShell {
+            buildInputs = [ pack.default ];
+          };
+          holesail-server = pkgs.mkShell {
+            buildInputs = [ pack.holesail-server ];
+          };
+          holesail-client = pkgs.mkShell {
+            buildInputs = [ pack.holesail-client ];
+          };
+        };      
+      }
+    );
 }
