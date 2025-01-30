638f5af35ca037ddc3e6450748db3f5a13875080
Vaios K <jjacke@proton.me>
2025-01-30 22:12:34 +0200

    Add files via upload

--- modules/holesail-client.nix
+++ modules/holesail-client.nix
@@ -2,37 +2,43 @@
 {
   config,
   lib,
-  pkgs ? import <nixpkgs> {},
+  pkgs,
   ...
 }:
+with lib;
 let
-  holesail = (import ./holesail.nix {inherit pkgs;});
+  holesail = (import ../holesail.nix {inherit pkgs;});
   cfg = config.services.holesail-client;
 in
 {
-  options.services.holesail-client = lib.mkOption {
-    type = lib.types.attrsOf (lib.types.submodule {
+  options.services.holesail-client = mkOption {
+    type = types.attrsOf (types.submodule {
       options = {
-        enable = lib.mkEnableOption "Enable this Holesail client instance.";
-        host = lib.mkOption {
-          type = lib.types.str;
+        enable = mkEnableOption "Enable this Holesail client instance.";
+        host = mkOption {
+          type = types.str;
           default = "127.0.0.1";
           description = "Host address to use for this Holesail client instance.";
         };
-        port = lib.mkOption {
-          type = lib.types.port;
+        port = mkOption {
+          type = types.port;
           default = 8989;
           description = "The port which this Holesail client instance should use.";
         };
-        udp = lib.mkOption {
-          type = lib.types.bool;
+        udp = mkOption {
+          type = types.bool;
           default = false;
           description = "Enable UDP instead of TCP.";
         };
-        connection-string = lib.mkOption {
-          type = lib.types.str;
+        connection-string = mkOption {
+          type = types.str;
           default = "";
           description = "The connection string to the Holesail server. If empty, there will be an error.";
+        };
+        connection-string-file = mkOption {
+          type = types.str;
+          default = "";
+          description = "The path to a file containing the connection string to the Holesail server. If null, the connection-string option will be used.";
         };
       };
     });
@@ -40,29 +46,29 @@ in
     default = {};
   };
 
-  config = lib.mkIf (lib.any (name: cfg.${name}.enable) (lib.attrNames cfg)) {
-    systemd.services = lib.genAttrs (lib.attrNames cfg) (name: 
+  config = mkIf (any (name: cfg.${name}.enable) (attrNames cfg)) {
+    systemd.services = genAttrs (attrNames cfg) (name: 
       let
         instanceCfg = cfg.${name};
       in
-        lib.mkIf instanceCfg.enable {
+        mkIf instanceCfg.enable {
           description = "Holesail client (${name})";
           wantedBy = [ "multi-user.target" ];
           after = [ "network.target" ];
-          serviceConfig = {
-            ExecStart = ''${holesail}/bin/holesail \
+          path = [ holesail ];
+          script = ''holesail \
               ${instanceCfg.connection-string} \
+              $(cat ${instanceCfg.connection-string-file}) \
               --port ${toString instanceCfg.port} \
               --host ${instanceCfg.host} \
               ${if instanceCfg.udp then "--udp" else ""}
-            '';
-            Type = "simple";
-            Restart = "always";
-            RestartSec = "10";
-          };
+          '';
+          serviceConfig.Type = "simple";
+          serviceConfig.Restart = "always";
+          serviceConfig.RestartSec = "10";
         }
     );
   };
 
-  meta.maintainers = with lib.maintainers; [ jjacke13 ];
+  meta.maintainers = with maintainers; [ jjacke13 ];
 }
