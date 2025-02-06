5eb1a6f9ab3dc51206a00352de6e9784f838c4be
Vaios K <jjacke@proton.me>
2025-02-06 08:16:23 +0200

    Update to holesail 1.10.0

--- holesail.nix
+++ holesail.nix
@@ -2,16 +2,16 @@
 
 pkgs.buildNpmPackage rec {
   pname = "holesail";
-  version = "1.9.2";
+  version = "1.10.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "holesail";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-Zk5fCNRa1HOMvstW5WbbNIrBMpCgRC1Z8mnB80MiIAg=";
+    hash = "sha256-eANDS4Avu6or+1XXRmM+rceZzjKchhoc5nbv1grpvwE=";
   };
 
-  npmDepsHash = "sha256-FAmJO1Jxm7S8JAXd5MJugIBNyO40s+51gelPMN87tkg=";
+  npmDepsHash = "sha256-TKxm8WdsJ6fbs8WBAN7yxNQoJ43fTQghD0KYGxRRd50=";
 
   npmPackFlags = [ "--ignore-scripts" ];
 
