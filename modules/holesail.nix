373534d79419253971e9d56b74f7cabba71b6ac8
jjacke13 <jjacke@proton.me>
2025-02-16 16:42:32 +0200

    update

--- modules/holesail.nix
+++ modules/holesail.nix
@@ -1,9 +1,13 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
     [ ./holesail-server.nix
       ./holesail-client.nix
       ./holesail-filemanager.nix
+    ];
+
+    environment.systemPackages = [
+      (import ../holesail.nix {inherit pkgs;})
     ];
 }
