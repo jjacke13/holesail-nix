4d73a7404645c6505ac84bf1b71d4936e6012138
Vaios K <jjacke@proton.me>
2025-08-16 16:13:22 +0300

    fix

--- holesail.nix
+++ holesail.nix
@@ -17,7 +17,7 @@ pkgs.buildNpmPackage rec {
 
   buildPhase = "echo 'No build phase required'";
   
-  meta = with lib; {
+  meta = with pkgs.lib; {
     description = "Holesail is a truly peer-to-peer network tunneling and reverse proxy software that supports both TCP and UDP protocols. 
         Holesail lets you share any locally running application on a specific port with third parties securely and with a single command.
         No static IP or port forwarding required.";
