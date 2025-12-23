31e2b6c3498a620293f0c939045e5d12726066d3
Vaios K <jjacke@proton.me>
2025-12-23 19:23:43 +0200

    update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Holesail";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     systems.url = "github:nix-systems/default-linux";
     flake-utils = {
       url = "github:numtide/flake-utils";
